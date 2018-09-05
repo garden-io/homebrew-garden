38e5792764931d5e038717b25b3776cdb93cd05a
Jon Edvald <edvald@gmail.com>
2018-09-05 14:57:05 -0700

    update to 0.5.0

--- Formula/garden-cli.rb
+++ Formula/garden-cli.rb
@@ -3,8 +3,8 @@ require "language/node"
 class GardenCli < Formula
   desc "A full-featured development framework for containers and serverless"
   homepage "https://github.com/garden-io/garden"
-  url "https://registry.npmjs.org/garden-cli/-/garden-cli-0.4.0.tgz"
-  sha256 "1f7a5a7c5428a6c77483f03d34a594c166346cdb6ed4d0f9387fa8871ef9e0d0"
+  url "https://registry.npmjs.org/garden-cli/-/garden-cli-0.5.0.tgz"
+  sha256 "36ecd7994e8780a7b54c294ae8282caa8881e1e290308389d9f26ced153bab28"
 
   depends_on "node"
   depends_on "rsync"
