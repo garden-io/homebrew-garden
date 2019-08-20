1a4fd360bf5a5d1e173ed94fac5e90f41b7a5d40
Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
2019-08-20 19:11:20 +0200

    update to 0.10.6

--- Formula/garden-cli.rb
+++ Formula/garden-cli.rb
@@ -1,9 +1,9 @@
 class GardenCli < Formula
   desc "Development engine for Kubernetes"
   homepage "https://garden.io"
-  url "https://github.com/garden-io/garden/releases/download/v0.10.5/garden-v0.10.5-macos-amd64.tar.gz"
-  version "0.10.5"
-  sha256 "e1cb8338bd7ad105779b787257f07ddf156e776bca4ae7e3a03d7b6145bcb726"
+  url "https://github.com/garden-io/garden/releases/download/v0.10.6/garden-v0.10.6-macos-amd64.tar.gz"
+  version "0.10.6"
+  sha256 "4be6ab2840cb2e5cf69dac16f2315b7d0204e94ab4568a036cb4fde37028f895"
 
   depends_on "rsync"
 
