2ff508bb086986bd9a041d582ce554d1e7a13ebd
Jon Edvald <edvald@gmail.com>
2019-08-21 09:16:08 +0200

    fix formula

--- Formula/garden-cli.rb
+++ Formula/garden-cli.rb
@@ -3,12 +3,12 @@ class GardenCli < Formula
   homepage "https://garden.io"
   url "https://github.com/garden-io/garden/releases/download/v0.10.6/garden-v0.10.6-macos-amd64.tar.gz"
   version "0.10.6"
-  sha256 "4be6ab2840cb2e5cf69dac16f2315b7d0204e94ab4568a036cb4fde37028f895"
+  sha256 "657c160fa269f1a8c265e1c5af7d3f801984c3f60d3b4f687ea9365f3b7ae7fc"
 
   depends_on "rsync"
 
   def install
-    libexec.install "garden", "fse.node", "static"
+    libexec.install "garden", "fsevents.node", "static"
     bin.install_symlink libexec/"garden"
   end
 
