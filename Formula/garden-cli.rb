c0d100eae5279eabfa1b7b0c4e4cb56c4757dc53
runner <runner@Mac-1488.local>
2020-01-24 10:25:30 +0000

    update to 0.11.1

--- Formula/garden-cli.rb
+++ Formula/garden-cli.rb
@@ -1,14 +1,14 @@
 class GardenCli < Formula
   desc "Development engine for Kubernetes"
   homepage "https://garden.io"
-  url "https://github.com/garden-io/garden/releases/download/v0.11.0/garden-v0.11.0-macos-amd64.tar.gz"
-  version "0.11.0"
-  sha256 "88831b7a7253f1f9514a7f5f381f07da7ed1a311bcf9b94b1e9898c8fc98b661"
+  url "https://github.com/garden-io/garden/releases/download/v0.11.1/garden-v0.11.1-macos-amd64.tar.gz"
+  version "0.11.1"
+  sha256 "870f8807970b2c836d30a962fd4be40e511dbaafcbe1111be49fdcb9c2a94905"
 
   depends_on "rsync"
 
   def install
-    libexec.install "garden", "fsevents.node", "static"
+    libexec.install "garden", "*.node", "static"
     bin.install_symlink libexec/"garden"
   end
 
