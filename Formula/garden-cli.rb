099be4189c9717cea21a7a30770555556800364f
Emanuele Libralato <10ko@users.noreply.github.com>
2022-06-02 15:33:32 +0200

    Bump garden-cli to 0.12.41.
    For more info: https://github.com/garden-io/garden

--- Formula/garden-cli.rb
+++ Formula/garden-cli.rb
@@ -1,9 +1,9 @@
 class GardenCli < Formula
   desc "Development engine for Kubernetes"
   homepage "https://garden.io"
-  url "https://github.com/garden-io/garden/releases/download/0.12.40/garden-0.12.40-macos-amd64.tar.gz"
-  version "0.12.40"
-  sha256 "b6a813ae851d750274a4968f7efac637ce7e07558f90abadcca37c8d01e30a11"
+  url "https://github.com/garden-io/garden/releases/download/0.12.41/garden-0.12.41-macos-amd64.tar.gz"
+  version "0.12.41"
+  sha256 "ac56a28d2efba2314e7103dc0729212e4a112fc5cb626d6ddd08c39b37871c0d"
 
   depends_on "rsync"
 
