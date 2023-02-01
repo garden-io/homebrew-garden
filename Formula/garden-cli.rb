acc0aa0cce17495b84767d225ebb12166a6b5ceb
Emanuele Libralato <10ko@users.noreply.github.com>
2023-02-01 15:34:09 +0100

    This is just a test. DO NOT MERGE
    You can safely close this PR.
    
    This is a test PR created by https://github.com/garden-io/garden/pull/3649
    
    TEST: Bump garden-cli to edge.

--- Formula/garden-cli.rb
+++ Formula/garden-cli.rb
@@ -1,9 +1,9 @@
 class GardenCli < Formula
   desc "Development engine for Kubernetes"
   homepage "https://garden.io"
-  url "https://github.com/garden-io/garden/releases/download/0.12.49/garden-0.12.49-macos-amd64.tar.gz"
-  version "0.12.49"
-  sha256 "3ffdb9583b316fd7eca860a34b31058b92c70bf1949dc9d0a0566d9ce8dcb123"
+  url "https://download.garden.io/core/edge/garden-edge-macos-amd64.tar.gz"
+  version "edge"
+  sha256 "ab264e0332a575bbdcdf4ee8456dba4b7f3e7c5cddd681e8bb1da5b0f2919b6a"
 
   depends_on "rsync"
 
