898c6c131c7db1145694baa021f883f0138c1915
TimBeyer <TimBeyer@users.noreply.github.com>
2023-09-12 09:37:58 +0000

    Bump garden-cli.rb to edge-bonsai
    This is just a test. DO NOT MERGE
    
    You can safely close this PR. This is a test PR created by https://github.com/garden-io/garden/pull/5062
    
    TEST: Bump {{formulaName}} to {{version}}.
    
    @TimBeyer Please close this pull request.

--- Formula/garden-cli.rb
+++ Formula/garden-cli.rb
@@ -1,11 +1,19 @@
 class GardenCli < Formula
   desc "Development engine for Kubernetes"
   homepage "https://garden.io"
-  url "https://download.garden.io/core/0.13.13/garden-0.13.13-macos-amd64.tar.gz"
-  version "0.13.13"
-  sha256 "45e7d55a1657b9d4af43c8a508fdfb0e1c48379829586446dba243a515fa434c"
+
+  version "edge-bonsai"
 
   depends_on "rsync"
+
+  # Determine architecture
+  if Hardware::CPU.arm?
+    url "https://download.garden.io/core/edge-bonsai/garden-edge-bonsai-macos-arm64.tar.gz"
+    sha256 "77bdb6e132402f3e4c470490b923cbed7b7b993c0598f57defc3ede126fd63de"
+  else
+    url "https://download.garden.io/core/edge-bonsai/garden-edge-bonsai-macos-amd64.tar.gz"
+    sha256 "7c4e4328d335ed685ae577f1523ee7a4632d4f7617cfe14ccafc3c79b3ba642c"
+  end
 
   def install
     libexec.install "garden", "fsevents.node", "static"
