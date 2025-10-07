49e09882bc117b268cee8e4c8974056fa00b0f2d
eysi09 <5373776+eysi09@users.noreply.github.com>
2025-10-07 13:30:19 +0000

    Bump garden-cli.rb to 0.14.9
    This PR has been generated by the publish-release workflow after the new release
    
    @edvald-garden Please review this PR carefully and merge once Garden should be released to Homebrew.

--- Formula/garden-cli.rb
+++ Formula/garden-cli.rb
@@ -2,15 +2,15 @@ class GardenCli < Formula
   desc "Development engine for Kubernetes"
   homepage "https://garden.io"
 
-  version "0.14.8"
+  version "0.14.9"
 
   # Determine architecture
   if Hardware::CPU.arm?
-    url "https://download.garden.io/core/0.14.8/garden-0.14.8-macos-arm64.tar.gz"
-    sha256 "80059eab8f6e184a61467a178c1828c17865dfc053596f8e1b2baff20be5c7fb"
+    url "https://download.garden.io/core/0.14.9/garden-0.14.9-macos-arm64.tar.gz"
+    sha256 "35aa0526c3b5f7a12816f50d3539b616166277eb3fdb5fb6b7629cca3d39ffda"
   else
-    url "https://download.garden.io/core/0.14.8/garden-0.14.8-macos-amd64.tar.gz"
-    sha256 "93b9d63f2a1da36c2a8f866137f75aca46a6a7b696bc031bc69ccbb3608331e2"
+    url "https://download.garden.io/core/0.14.9/garden-0.14.9-macos-amd64.tar.gz"
+    sha256 "7d5176154a805b4bb5440beff523fe9ebe30cce2e700fca84aedf4b32279e652"
   end
 
   def install
